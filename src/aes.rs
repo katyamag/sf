3b1dd59052ac8699fe0d82f08f4035096ac35f8b
ky <pupupu0701@gmail.com>
2021-01-09 00:21:15 +0900

    add unit test for function that gets file list
    Signed-off-by: ky <pupupu0701@gmail.com>

--- src/aes.rs
+++ src/aes.rs
@@ -24,7 +24,7 @@ pub trait AES<'a> {
 }
 
 fn get_file_list(input_dir: &PathBuf, output_dir: &PathBuf, file_type: &str) -> Result<Vec<std::string::String>, SfError> {
-    let list = std::fs::read_dir(input_dir)?
+    let mut list = std::fs::read_dir(input_dir)?
         .filter_map(Result::ok)
         .map(|e| e.path())
         .filter(|e| e.display().to_string().contains(file_type))
@@ -35,27 +35,17 @@ fn get_file_list(input_dir: &PathBuf, output_dir: &PathBuf, file_type: &str) -> 
             .to_string())
         .collect::<Vec<_>>();
 
-    println!("{:?}", list);
+    let check_file_type = match file_type {
+        ".enc" => ".mp4",
+        ".mp4" => ".enc",
+        _ => return Err(SfError::new("invalid file_type for file list".to_owned()))
+    };
 
-    // TODO
-    // for f in &list {
-    //     let filename = f.file_name().expect("failed to get filename")
-    //         .to_str().expect("failed to convert to str")
-    //         .split(".")
-    //         .collect::<Vec<&str>>()[0]
-    //         .to_string();
-
-    //     let check_file_type = match file_type {
-    //         ".enc" => ".mp4",
-    //         ".mp4" => ".enc"
-    //     };
-
-    //     if Path::new(&format!("{}/{}{}", output_dir.display(), filename, check_file_type)).exists() {
-    //         continue
-    //     }
-
-    //     println!("{}", filename);
-    // }
+    for (i, f) in list.clone().iter().enumerate() {
+        if Path::new(&format!("{}/{}{}", output_dir.display(), f, check_file_type)).exists() {
+            list.remove(i);
+        }
+    }
 
     Ok(list)
 }
@@ -72,6 +62,11 @@ impl<'a> AES<'a> for OpenSSLAesCommand<'a> {
 
     fn encrypt(&self) -> SfResult {
         let file_list = get_file_list(self.input, self.output, ".mp4").expect("failed to get file list");
+
+        if file_list.len() == 0 {
+            println!("non target file");
+            return Ok(())
+        }
 
         let mut pb = Arc::new(Mutex::new(ProgressBar::new(file_list.len() as u64)));
         let mfile_list = Arc::new(Mutex::new(file_list));
@@ -155,6 +150,12 @@ impl<'a> AES<'a> for OpenSSLAesCommand<'a> {
 
     fn decrypt(&self) -> SfResult {
         let file_list = get_file_list(self.input, self.output, ".enc").expect("failed to get file list");
+
+        if file_list.len() == 0 {
+            println!("non target file");
+            return Ok(())
+        }
+
         let mut pb = Arc::new(Mutex::new(ProgressBar::new(file_list.len() as u64)));
         let mfile_list = Arc::new(Mutex::new(file_list));
         let mut thread_handlers = vec![];
@@ -213,5 +214,59 @@ impl<'a> AES<'a> for OpenSSLAesCommand<'a> {
         }
 
         Ok(())
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::fs;
+    use std::fs::File;
+    use tempfile::tempdir;
+
+    #[test]
+    fn test_get_file_list_when_encrypt() {
+        let dir = tempdir().unwrap();
+
+        let tmp_file_list = vec![
+            "0057a2b4-fb8c-46bb-8133-ef9920b7bced.mp4",
+            "c7dc8644-8559-44f5-96e8-ecc035067856.mp4",
+            "0057a2b4-fb8c-46bb-8133-ef9920b7bced.enc"
+        ];
+
+        for f in &tmp_file_list {
+            File::create(dir.path().join(f)).unwrap();
+        }
+
+        let as_pathbuf = &dir.into_path();
+
+        let file_list = get_file_list(&as_pathbuf, &as_pathbuf, ".mp4").expect("fail");
+
+        assert_eq!(file_list.len(), 1);
+
+        fs::remove_dir_all(as_pathbuf).unwrap();
+    }
+
+    #[test]
+    fn test_get_file_list_when_decrypt() {
+        let dir = tempdir().unwrap();
+
+        let tmp_file_list = vec![
+            "0057a2b4-fb8c-46bb-8133-ef9920b7bced.enc",
+            "c7dc8644-8559-44f5-96e8-ecc035067856.enc",
+            "0057a2b4-fb8c-46bb-8133-ef9920b7bced.mp4"
+        ];
+
+        for f in &tmp_file_list {
+            File::create(dir.path().join(f)).unwrap();
+        }
+
+        let as_pathbuf = &dir.into_path();
+
+        let file_list = get_file_list(&as_pathbuf, &as_pathbuf, ".enc").expect("fail");
+
+        assert_eq!(file_list.len(), 1);
+
+        fs::remove_dir_all(as_pathbuf).unwrap();
     }
 }
